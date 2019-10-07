51fe2b858afea771570ccea58a6f2b83c4a4da3e
Stephen Blais <stephen@s-blais.com>
2019-10-07 17:24:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,9 +16,9 @@ class Student
     end
   end
 
-  def self.find_by_name(name:)
+  def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql, name).each do |row|
     self.new_from_db(row)
     end
   end
