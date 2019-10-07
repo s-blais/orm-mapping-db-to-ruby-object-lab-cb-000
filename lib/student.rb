957b97560c411f9eb5738ecc04d2ca5968b1cad9
Stephen Blais <stephen@s-blais.com>
2019-10-07 17:54:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,4 +78,11 @@ class Student
     end.first
   end
 
+  def self.all_students_in_grade_X (grade)
+    sql = "SELECT * FROM students WHERE grade = ?"
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
 end
